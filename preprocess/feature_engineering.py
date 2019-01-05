b1d5b874f44a26b9a13ead1c36245a75ff936c85
Jin Li <jinli11@cs.uchicago.edu>
2019-01-05 17:53:53 -0500

    integrated pipeline with sklearn

--- preprocess/feature_engineering.py
+++ preprocess/feature_engineering.py
@@ -15,9 +15,18 @@ Outlier Removals:
     z-score: parametric, usually 2.5, 3, 3.5
     Dbscan:  https://towardsdatascience.com/a-brief-overview-of-outlier-detection-techniques-1e0b2c19e561
     Isolation Forest:
+Dealing With Irregular Values:
+    replace by mean or median: problem is that it doesn't account for uncertainty in imputations and doesn't factor in correlations between features
+feature combination:
+    is there a way to combine features to make more insight? I don't see any reason right now.
 
 Categorical:
 If there's order, we can convert the categories into integers.
 If there's no order, use one hot encoding.
 If there's too many labels, group them into bins or use dimension reduction.
+If there's irregular labels:
+    replace by most frequent: problem is that it introduces biases
+If there's missing values:
+    replace by most common.
+    Note that this is very tricky, and I really need to compare before and afters for training to see if this works.
 """
